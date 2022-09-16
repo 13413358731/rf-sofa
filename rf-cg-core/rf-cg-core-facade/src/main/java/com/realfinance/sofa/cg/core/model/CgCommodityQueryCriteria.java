d40211e19a1fbbe3a2469e7d4ee3e195c23ebcff
bin <1556819418@qq.com>
2022-09-16 14:32:49 +0800

    第一次提交CgCommodityQueryCriteria

---
+++ rf-cg-core/rf-cg-core-facade/src/main/java/com/realfinance/sofa/cg/core/model/CgCommodityQueryCriteria.java
@@ -0,0 +1,70 @@
+package com.realfinance.sofa.cg.core.model;
+
+public class CgCommodityQueryCriteria {
+
+    /**
+     * 项目名称模糊
+     */
+    private String commodityNameLike;
+
+//    /**
+//     * 项目编码模糊
+//     */
+//    private String projectNoLike;
+
+//    private LocalDate startDateBefore;
+//
+//    private LocalDate startDateAfter;
+//
+//    private LocalDate expireDateBefore;
+//
+//    private LocalDate expireDateAfter;
+
+    public String getCommodityNameLike() {
+        return commodityNameLike;
+    }
+
+    public void setCommodityNameLike(String commodityNameLike) {
+        this.commodityNameLike = commodityNameLike;
+    }
+
+//    public String getProjectNoLike() {
+//        return projectNoLike;
+//    }
+//
+//    public void setProjectNoLike(String projectNoLike) {
+//        this.projectNoLike = projectNoLike;
+//    }
+//
+//    public LocalDate getStartDateBefore() {
+//        return startDateBefore;
+//    }
+//
+//    public void setStartDateBefore(LocalDate startDateBefore) {
+//        this.startDateBefore = startDateBefore;
+//    }
+//
+//    public LocalDate getStartDateAfter() {
+//        return startDateAfter;
+//    }
+//
+//    public void setStartDateAfter(LocalDate startDateAfter) {
+//        this.startDateAfter = startDateAfter;
+//    }
+//
+//    public LocalDate getExpireDateBefore() {
+//        return expireDateBefore;
+//    }
+//
+//    public void setExpireDateBefore(LocalDate expireDateBefore) {
+//        this.expireDateBefore = expireDateBefore;
+//    }
+//
+//    public LocalDate getExpireDateAfter() {
+//        return expireDateAfter;
+//    }
+//
+//    public void setExpireDateAfter(LocalDate expireDateAfter) {
+//        this.expireDateAfter = expireDateAfter;
+//    }
+}
